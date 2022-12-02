020b5f20a37dcb4daec8f4ef10c3df0ec1be7d62
javie <javie@DESKTOP-OFFFDRF>
2022-12-02 16:23:07 -0500

    PRIMER COMMIT

---
+++ src/git_mariel/GIT_MARIEL.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_mariel;
+
+/**
+ *
+ * @author javie
+ */
+public class GIT_MARIEL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
